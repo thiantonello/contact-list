2e42079bd393018cb3f622e92c7b8059db4dd961
Thiago Antonello Vargas <thiantonello@gmail.com>
2022-02-15 01:45:04 -0300

    test: add new tests and correct old ones
    validations are now all tested:
    - includes uniqueness tests for all classes
    
    add tests for PhoneNumber callbacks
    
    tests now are more specific and focused on the raised errors

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -7,20 +7,17 @@ class PhoneNumber < ApplicationRecord
   validates :label, presence: true
   enum label: { home: 0, work: 1, other: 2 }
   validates :main, inclusion: [true, false]
-  validate :validate_only_one_phone_number_is_main
+  validate :only_one_phone_number_is_main
 
   before_save :set_main_if_phone_numbers_is_empty
   before_save :ensure_only_one_phone_number_is_main
 
   private
 
-  def validate_only_one_phone_number_is_main
-    main_counter = 0
+  def only_one_phone_number_is_main
+    return unless contact.phone_numbers.where(main: true).count > 1
 
-    contact.phone_numbers.each do |phone_number|
-      main_counter += 1 if phone_number.main?
-    end
-    errors.add(:main, 'Only one phone_number can be main') if main_counter > 1
+    errors.add(:main, 'Only one phone_number can be main')
   end
 
   def ensure_only_one_phone_number_is_main
